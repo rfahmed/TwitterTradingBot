8907fda402e8acd976fdbf1eff70d55a2b9c23ac
Justin Chen <jc10182@stern.nyu.edu>
2020-08-16 02:54:59 -0400

    Set up a limited query searcher for now

--- src/TwitterApi.py
+++ src/TwitterApi.py
@@ -1,64 +1,70 @@
-# There's a chance that the hardcoded bearer token in setup.py can change.
-# In which case we need to code some lines to get the bearer token based on the API keys
-# look fat the oauth2 pages on twitter api webpages
+# This version doesn't have the time function implemented
+# and it only retrieves queries from Andrew Yang
+#
+# This is just a recent search function right now
+# So the main function needs to constantly call the function every (x) minutes
+# can implement livestreaming later, but will probably not be needed
 
 import requests
 import setup
-import os
-import json
-from twitter import api
-import tweepy
-from tweepy import Stream
-from tweepy import OAuthHandler
-
-class MyStreamListener(tweepy.StreamListener):
-    def on_data(self, data):
-        print(data)
-        return True
-    def on_error(self, status):
-        print(status)
+import pandas as pd
 
 
-listener = MyStreamListener()
-auth = OAuthHandler(setup.twitterApiKey, setup.twitterSecretKey)
-auth.set_access_token(setup.twitterAccessToken, setup.twitterAccessSecret)
-stream = Stream(auth, listener)
-stream.filter(track=["Walgreens -is:retweet"])
+def main():
+    print(searchTwitter(query="it", time="0"))
 
-def search():
-    #authorization for the twitterapi endpoint
-    headers = {
-        "Authorization": "Bearer " + setup.twitterBearerToken
-    }
-    #these are the parameters for search/stream
-    payload = {
-        "query" : "Walgreens -is:retweet"
-    }
-    r = requests.get("https://api.twitter.com/2/tweets/search/recent", params=payload, headers=headers)
-    print(r.json())
 
-def stream():
+# format: searchTwitter(query=(), time=(),  )
+# query is a string, time is int minutes,
+# if you want an exact query match, you need to use single quotes "'exact keyword'"
+def searchTwitter(**kwargs):
+    # checks for the necessary function arguments
+    try:
+        kwargs["query"]
+    except KeyError:
+        raise Exception("searchTwitter() Must have a query. Usage: searchTwitter(query = str)")
+
+    try:
+        kwargs["time"]
+    except KeyError:
+        raise Exception("searchTwitter() Must have a time. Usage searchTwitter(time = int)")
+
     # authorization for the twitterapi endpoint
     headers = {
         "Authorization": "Bearer " + setup.twitterBearerToken
     }
-    #This creates a filtered stream based on the Key word Walgreens
-    data = {
-        "add": [{"value": "Walgreens -is:retweet", "tag": "Walgreen tweets"}]
+
+    # these are the parameters for search/stream
+    fixed_query_operator = " from:AndrewYang -is:retweet lang:en"
+    payload = {
+        "query": kwargs["query"] + fixed_query_operator,
+        "start_time": "2020-08-15T00:00:01Z",
+        "tweet.fields": "created_at,attachments"
     }
-    param = {
-        "tweet.fields": "created_at"
-    }
-    tempheader = {
-        "Content-type": "application/json",
-        "Authorization": "Bearer " + setup.twitterBearerToken
-    }
-    # requests.get("https://api.twitter.com/1.1/application/rate_limit_status.json?resources=help,users,search,statuses", data=data, headers=headers)
-    r = requests.get("https://api.twitter.com/2/tweets/search/stream", headers=headers)
-    requests.post("https://api.twitter.com/2/tweets/search/stream/rules", stream=True, data=data, headers=tempheader)
-    x = requests.get("https://api.twitter.com/2/tweets/search/stream/rules", stream=True, headers=headers)
-    print("test")
-    print(x.json())
+    # makes sure the query is less than the twitter query limit
+    if len(kwargs["query"]+fixed_query_operator) > 512:
+        raise Exception("Total query is too long (>512 characters)")
+
+    # retrieves the first Twitter JSON object response and creates a dataframe from it
+    r = requests.get("https://api.twitter.com/2/tweets/search/recent", params=payload, headers=headers)
+    print(r.json())
+    df = pd.DataFrame(r.json()["data"])
+
+    # Iterates to add all response pages to the dataframe
+    isLastPage = False
+    while not isLastPage:
+        try:
+            next_token = r.json()["meta"]["next_token"]
+            payload["next_token"] = next_token
+            r = requests.get("https://api.twitter.com/2/tweets/search/recent", params=payload, headers=headers)
+            df = df.append(pd.DataFrame(r.json()["data"]), ignore_index=True)
+        except KeyError:
+            isLastPage = True
+
+    # returns the dataframe
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+    return df
 
 
-
+main()
