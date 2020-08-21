5a87325a6918d8b88d22ecb38de432ab4e1659b2
Justin Chen <jc10182@stern.nyu.edu>
2020-08-20 23:19:06 -0400

    Final Version of the Twitter Search Bot

--- src/TwitterApi.py
+++ src/TwitterApi.py
@@ -2,21 +2,31 @@
 # So the main function needs to constantly call the function every (x) minutes
 # can implement livestreaming later, but will probably not be needed
 
-# Output:
-# Dataframe: index, id, text,
-# "entities" is a dict of hashtags, urls, cashtags, and annotations
-# "context_annotations" is confusing. It tries to find subjects in the text and classify them
-#       like "stock" "hospitality" "travel" "personal finance". Sometimes non-domain things as well
-#       also it can sometimes pick out specific company names
-# If there are no tweets, it will output a string "No New Tweets"
-# All the columns with a . in them are going to be lists
-
-# I finished the parse function in this version
-# I just need to check this against all domain paths
-# Check if indexes line up
-# Attach the returned dfs to the original df
-# Do the same for the expansion tweet df
-# Clean up the comments
+# Output DataFrame Column Headers:
+# id: unique id given to every tweet
+# text: text of tweet
+# Created_at: date created
+# context_annotations.domain.name: annotations of the content of the tweet
+# context_annotations.domain.descriptions describes what each domain.name means. i.e. brand vertical = an industry
+# context_annotations.entity.name: tries to identify nouns. (common noun or proper nouns). like companies
+# context_annotations.entity.description: describes the industry/role/position of the entity
+# entities.annotations.probability: probability of the following entity being true
+# entities.annotations.type: entity type (person, place, organization, etc.)
+# entities.annotations.normalized_text: if it exists, its the specific above (i.e. company -> Google)
+# entities.urls.url: urls in the tweet
+# entities.hashtags.tag: hashtags in the tweet
+# entities.mentions.username: mentions in the tweet
+# entities.cashtags.tag: cashtags mentioned in the tweet
+# possibly_sensitive: nsfw or not
+# referenced_tweets.type: if the tweet references another, how (retweet with comment, reply to, quote, etc.)
+# referenced_tweets.id: twitter id of the referenced tweet
+# There will be more columns with a prefix "ref_twt_" which signfies all the tweet data as above, except for the
+# referenced tweet, if there is one.
+#
+# If there are no tweets within the timeframe, it will output a string "No New Tweets"
+# Column headers with a "." in them have list datatypes
+# no value is marked by NaN
+# Sometimes referenced_tweet data is missing. That is because the referenced_tweet may be from a private account
 
 import requests
 import setup
@@ -25,9 +35,7 @@ import datetime
 import numpy as np
 
 def main():
-    searchTwitter(query="Walgreens", timeframe=35)
-    #print(searchTwitter(query="Walgreens", timeframe=10))
-
+    print(searchTwitter(query="Walgreens", timeframe=10))
 
 # format: searchTwitter(query=(str), timeframe=(int))
 # if you want an exact query match, you need to use single quotes "'exact keyword'"
@@ -78,7 +86,6 @@ def searchTwitter(**kwargs):
     except KeyError:
         pass
 
-
     # Twitter data comes in pages
     # This pages through and adds all addition tweets and expansion objects to their respective dfs
     isLastPage = False
@@ -96,33 +103,55 @@ def searchTwitter(**kwargs):
             isLastPage = True
 
     # Changes the header of the referenced_tweet table so you know which data is from the referenced_tweet (ref_twt)
-    expansiondf = expansiondf.rename(columns={"id": "ref_twt_id", "text": "ref_twt_text",
+    expansiondf = expansiondf.rename(columns={"id": "referenced_tweets.id", "text": "ref_twt_text",
                                               "created_at": "ref_twt_created_at",
                                               "context_annotations": "ref_twt_context_annotations",
                                               "entities": "ref_twt_entities",
                                               "possibly_sensitive": "ref_twt_possibly_sensitive",
                                               "referenced_tweets": "ref_twt_referenced_tweets"})
 
-    # Parses the referenced_tweet object in df into two columns: ref_type and ref_twt_id
-    # That way we can attach df and expansiondf side by side and use the ref_twt_id to line them up
+    # This section parses out relevant data from Context_annotations, entities, referenced_tweets object columns
+    # Referenced_tweets
+    df = replaceObjectColumn(df, "referenced_tweets",
+                             parseObjectColumn(df, "referenced_tweets",
+                                               "referenced_tweets.type", "referenced_tweets.id"))
+    # Entities
+    df = replaceObjectColumn(df, "entities",
+                             parseObjectColumn(df, "entities", "entities.annotations.probability",
+                                               "entities.annotations.type", "entities.annotations.normalized_text",
+                                               "entities.urls.url", "entities.hashtags.tag",
+                                               "entities.mentions.username", "entities.cashtags.tag"))
+    # Context_annotations
+    df = replaceObjectColumn(df, "context_annotations",
+                             parseObjectColumn(df, "context_annotations", "context_annotations.domain.name",
+                                               "context_annotations.domain.description",
+                                               "context_annotations.entity.name",
+                                               "context_annotations.entity.description"))
+    # ref_twt_referenced_tweets
+    expansiondf = replaceObjectColumn(expansiondf, "ref_twt_referenced_tweets",
+                             parseObjectColumn(expansiondf, "ref_twt_referenced_tweets",
+                                               "ref_twt_referenced_tweets.type", "ref_twt_referenced_tweets.id"))
+    # ref_twt_entities
+    expansiondf = replaceObjectColumn(expansiondf, "ref_twt_entities",
+                             parseObjectColumn(expansiondf, "ref_twt_entities", "ref_twt_entities.annotations.probability",
+                                               "ref_twt_entities.annotations.type", "ref_twt_entities.annotations.normalized_text",
+                                               "ref_twt_entities.urls.url", "ref_twt_entities.hashtags.tag",
+                                               "ref_twt_entities.mentions.username", "ref_twt_entities.cashtags.tag"))
+    # ref_twt_context_annotations
+    expansiondf = replaceObjectColumn(expansiondf, "ref_twt_context_annotations",
+                             parseObjectColumn(expansiondf, "ref_twt_context_annotations", "ref_twt_context_annotations.domain.name",
+                                               "ref_twt_context_annotations.domain.description",
+                                               "ref_twt_context_annotations.entity.name",
+                                               "ref_twt_context_annotations.entity.description"))
+
+    # This links up the main df and expansiondf by referenced_tweets.id so it's one
+    df = df.merge(expansiondf, on="referenced_tweets.id", how="left", suffixes=(None, '_y'))
+
+    # Table Display
     pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 1000)
-    pd.set_option('max_colwidth', 400)
-    print(df)
-    print(parseObjectColumn(df, "entities", "entities.urls.url", "entities.hashtags.tag"))
+    pd.set_option('display.width', 3000)
+    pd.set_option('max_colwidth', 100)
 
-    # Parses the object columns into distinct columns
-    # i.e. takes a dict of entities and seperates them into columns of "hashtags", "cashtags", "mentions", etc.
-        #parseObjectColumn(df["entities"], "entities.hashtag", "test2", "test3")
-
-    # formats and returns the dataframe
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 1000)
-
-    #df = df[["id", "text", "created_at", "entities"]]
-
-    #print(expansiondf)
-    #print(df)
     return df
 
 # This takes a column of objects and separates them out into distinct columns
@@ -146,7 +175,7 @@ def parseObjectColumn(df, header, *args):
                 # An object may have many pieces of data of the same data type so our element will be a list
                 element = []
                 # If there's nothing in the element then just move on
-                if not pd.isnull(row).all():
+                try:
                     # This takes in the args and splits it into a list of the key names
                     address = args[i].split(".")
                     # Twitter's context_annotations object is a list of dicts. So this for loop makes
@@ -161,7 +190,7 @@ def parseObjectColumn(df, header, *args):
                             pass
                     # Adds element to the templist (aka a column)
                     templist.append(element)
-                else:
+                except TypeError:
                     templist.append(np.nan)
             # Adds the templist (column) to the data list
             data.append(templist)
@@ -174,7 +203,7 @@ def parseObjectColumn(df, header, *args):
             templist = []
             for row in df.loc[:, header]:
                 element = []
-                if not pd.isnull(row):
+                try:
                     address = args[i].split(".")
                     try:
                         for j in range(len(row[address[1]])):
@@ -182,7 +211,7 @@ def parseObjectColumn(df, header, *args):
                         templist.append(element)
                     except KeyError:
                         templist.append(np.nan)
-                else:
+                except TypeError:
                     templist.append(np.nan)
             data.append(templist)
     elif header == "referenced_tweets" or header == "ref_twt_referenced_tweets":
@@ -192,12 +221,12 @@ def parseObjectColumn(df, header, *args):
         for i in range(len(args)):
             templist = []
             for row in df.loc[:, header]:
-                element = []
-                if not pd.isnull(row):
+                try:
+                    address = args[i].split(".")
                     for j in range(len(row)):
-                        element.append(row[j][args[i]])
+                        element = row[j][address[1]]
                     templist.append(element)
-                else:
+                except TypeError:
                     templist.append(np.nan)
             data.append(templist)
     else:
@@ -205,24 +234,27 @@ def parseObjectColumn(df, header, *args):
 
     # This takes the 2xlen(args) list and enters into a dataframe such that the column headers is args
     # This new dataframe is what will be slapped out to the side of the original df
-    # This for args loop just to create new titles based on the old column name + the title of the individual data
-    # Only really needed for referenced_tweets so the else section is normal
-    if header == "referenced_tweets" or header == "ref_twt_referenced_tweets":
-        newheader = []
-        for arg in args:
-            newheader.append(header+"."+arg)
-        newdf = pd.DataFrame(index=range(0, len(df.loc[:, header])), columns=newheader)
-        for i in range(len(df.loc[:, header])):
-            for j in range(len(args)):
-                newdf.loc[i, newheader[j]] = data[j][i]
-    else:
-        newdf = pd.DataFrame(index=range(0, len(df.loc[:, header])), columns=args)
-        for i in range(len(df.loc[:, header])):
-            for j in range(len(args)):
-                newdf.loc[i, args[j]] = data[j][i]
+    # This for loop just to create new titles based on the data domain path
+    newdf = pd.DataFrame(index=range(0, len(df.loc[:, header])), columns=args)
+    for i in range(len(df.loc[:, header])):
+        for j in range(len(args)):
+            newdf.loc[i, args[j]] = data[j][i]
 
     return newdf
 
+# This function replaces the object column from the old dataframe with the parsed dataframe from parseObjectColumn()
+# It takes in the old df, the object column to delete, and the df with the new data to concat
+def replaceObjectColumn(df, columnheader, parsed_df):
+    if type(columnheader) is not str:
+        raise Exception ("Second argument of replaceObjectColumn() must be a str")
+    else:
+        # This section adds the parsed_df at the spot the old objectcolumn was
+        indexloc = df.columns.get_loc(columnheader)
+        headers = list(parsed_df.columns)
+        for i in range(len(parsed_df.columns)):
+            df.insert(indexloc+i+1,headers[i], parsed_df.loc[:, headers[i]])
+        del df[columnheader]
+    return df
 
 # Gets the UTC time var minutes ago in ISO 8601/RFC 3339 format, returns a str
 def getOldTime(minutes):
